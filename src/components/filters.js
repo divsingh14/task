6e0f23ec2fc10a6e1df07141ad91e2beb25bbd6f
Divyansh <divsingh14@yahoo.in>
2020-07-06 20:05:47 +0530

    modifications

--- src/components/filters.js
+++ src/components/filters.js
@@ -4,10 +4,10 @@ class SearchFilter extends React.Component{
     render(){
         return(
             <div className="row mt-3 filter-div">
-                <div className="col-sm-9">
+                <div className="col-lg-8 col-md-8 col-sm-8 ">
                     <p><span className="filter-title">Selected Filters :  </span><span className="filter">Continental <a href="/" className="ml-1"><i className="fas fa-times-circle"></i></a> </span></p>
                 </div>
-                <div className="col-sm-3 text-right">
+                <div className="col-lg-4 col-md-4 col-sm-4 text-right">
                     <p>
                         <span className="filter-title">Sort by :</span>
                         <select name="" className="filter mx-1">
--- src/components/templates/quantity.js
+++ src/components/templates/quantity.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const QuantityButton = () =>{
     return(
-        <div className="col qty">
-            <button className="qty-icon">-</button>
-            <input type="text" value='4' className="qty-value" readOnly />
-            <button className="qty-icon">+</button>
-        </div>
+            <div className="d-flex qty">
+                <button className="qty-icon">-</button>
+                <input type="text" value='4' className="qty-value" readOnly />
+                <button className="qty-icon">+</button>
+            </div>
     );
 };
 
